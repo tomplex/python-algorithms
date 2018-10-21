fe0adf6d08ae2ea28bd39deea97bd03e9cea8623
Tom Caruso <carusot42@gmail.com>
2018-10-21 17:01:11 -0400

    Bubble sort tests & implementations

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -1,5 +1,25 @@
+"""
+Implementations of the bubble sort algorithm.
 
+"""
 
 
 def bubble_sort(unsorted):
+    length = len(unsorted)
+    for i in range(length):
+        for idx in range(length - i - 1):
+            if unsorted[idx] > unsorted[idx + 1]:
+                unsorted[idx], unsorted[idx + 1] = unsorted[idx + 1], unsorted[idx]
+
     return unsorted
+
+
+def recursive_bubble_sort(unsorted):
+    for idx, num in enumerate(unsorted):
+        try: 
+            if unsorted[idx+1] < num:
+                unsorted[idx], unsorted[idx + 1] = num, unsorted[idx + 1]
+                bubble_sort(unsorted)
+        except IndexError: 
+            pass
+    return unsorted 
--- test/test_sort.py
+++ test/test_sort.py
@@ -1,6 +1,15 @@
 import pytest
+import random
 
 from sort import bubble_sort
+
+one_to_100 = list(range(1, 100))
+
+
+def shuffle_list(input_list):
+    new = list(input_list)
+    random.shuffle(new)
+    return new
 
 
 @pytest.mark.parametrize('unsorted,expected', [
@@ -9,7 +18,26 @@ from sort import bubble_sort
     ),
     (
         [4, 7, 2], [2, 4, 7]
+    ),
+    (
+        shuffle_list(one_to_100), one_to_100
     )
 ])
 def test_bubble_sort(unsorted, expected):
     assert expected == bubble_sort.bubble_sort(unsorted)
+
+
+@pytest.mark.parametrize('unsorted,expected', [
+    (
+        [6, 5, 4, 3, 2, 1], [1, 2, 3, 4, 5, 6]
+    ),
+    (
+        [4, 7, 2], [2, 4, 7]
+    ),
+    (
+        shuffle_list(one_to_100), one_to_100
+    )
+])
+def test_recursive_bubble_sort(unsorted, expected):
+    assert expected == bubble_sort.recursive_bubble_sort(unsorted)
+
